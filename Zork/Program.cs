ed5b82d33df2d815ff5d3f6892867862d03d8557
KRB3AST <59933252+KRB3AST@users.noreply.github.com>
2021-09-13 14:28:04 -0400

    Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,24 +4,88 @@ namespace Zork
 {
     class Program
     {
+
+        private static string Location
+        {
+            get
+            {
+                return Rooms[LocationColumn];
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
+            while (true)
             {
-                Console.WriteLine("Thank you for playing.");
+                Console.Write($"{Location}\n> ");
+                Commands command = ToCommand(Console.ReadLine().Trim());
+                if (command == Commands.QUIT)
+                {
+                    break;
+                }
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = ("Thank you for playing");
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:             
+                    case Commands.WEST:
+                        outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        break;
+                       
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
             }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+
+            Console.WriteLine("Finished.");
+
         }
+
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST when LocationColumn < Rooms.Length - 1:              
+                        LocationColumn++;
+                        didMove = true;
+                        break;
+
+                case Commands.WEST when LocationColumn > 0:                   
+                    LocationColumn--;
+                    didMove = true; 
+                    break;
+            }
+
+            return didMove;
+        }
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands command) ? command : Commands.UNKNOWN;
+        }
+
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int LocationColumn = 1;
     }
+    
 }
