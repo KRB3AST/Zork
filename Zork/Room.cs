c26dad9cbeeeb9160cb02e13d00c3c3fee8dc471
KRB3AST <59933252+KRB3AST@users.noreply.github.com>
2021-09-22 14:27:02 -0400

    Zork 3.1

---
+++ Zork/Room.cs
@@ -0,0 +1,16 @@
+﻿namespace Zork
+{
+
+    public class Room
+    {
+        public string Name { get; }
+
+        public string Description { get; set; }
+
+        public Room(string name= null, string description = null)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
